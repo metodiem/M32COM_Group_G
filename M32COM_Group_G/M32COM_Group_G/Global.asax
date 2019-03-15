52cb1c71cadbc24a8d08c68fa0162e9ec8ccebf0
Martin Metodiev <metodiem@uni.coventry.ac.uk>
2019-03-15 12:39:52 +0000

    New MVC Project

---
+++ M32COM_Group_G/M32COM_Group_G/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="M32COM_Group_G.MvcApplication" Language="C#" %>
---
+++ M32COM_Group_G/M32COM_Group_G/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(M32COM_Group_G.Startup))]
+namespace M32COM_Group_G
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
