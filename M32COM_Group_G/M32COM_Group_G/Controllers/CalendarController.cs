5f63c7113c5dbb7d95f3f79331fb4e85c3928cc7
Martin Metodiev <metodiem@uni.coventry.ac.uk>
2019-03-19 00:07:29 +0000

    Calendar Added

---
+++ M32COM_Group_G/M32COM_Group_G/Controllers/CalendarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M32COM_Group_G.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public JsonResult GetEvents()
+        //{
+        //    using (MyDatabaseEntities dc = new MyDatabaseEntities())
+        //    {
+        //        var events = dc.Events.ToList();
+        //        return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        //    }
+        //}
+    }
+}
---
+++ M32COM_Group_G/M32COM_Group_G/Controllers/PromotionalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M32COM_Group_G.Controllers
+{
+    public class PromotionalController : Controller
+    {
+        // GET: Promotional
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
