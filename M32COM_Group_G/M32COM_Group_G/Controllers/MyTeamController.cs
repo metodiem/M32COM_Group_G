086066b15fe700081130ee82effb874cca65da0a
Jason Evans <evansj39@uni.coventry.ac.uk>
2019-03-21 18:22:23 +0000

    updated files

---
+++ M32COM_Group_G/M32COM_Group_G/Controllers/MyTeamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M32COM_Group_G.Controllers
+{
+    public class MyTeamController : Controller
+    {
+        // GET: MyTeam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
